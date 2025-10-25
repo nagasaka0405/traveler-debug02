91b1d865cf429b444444aee2386874d9916f3faa
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-10-25 11:56:26 +0900

    [add]ポストIDカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_12_004135) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_25_025330) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_12_004135) do
     t.datetime "updated_at", null: false
     t.string "profile"
     t.string "profile_image_id"
+    t.integer "post_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
