5c9ea79f7d1ec224f571f21bdef1637b9cc7a505
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-10-26 11:55:22 +0900

    [fix]エラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
     def create
-        post = Post.find(params[:post_id])
+       post = Post.find(params[:post_id])
         @comment = current_user.comments.new(comment_params)
+        @comment.post_id = post.id
         if @comment.save
             redirect_to post_path(post)
         else
