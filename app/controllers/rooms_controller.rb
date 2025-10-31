860321213a67eaeceb3bc27c7ac9c46b2bec05b1
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-11-01 06:22:48 +0900

    [add]DM機能（roomコントローラー）

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,5 +2,9 @@ class RoomsController < ApplicationController
     before_action :authenticate_user!
 
     def create
-        @room = Room.create(user_id: corrent_user.id)
+        @room = Room.create(user_id: current_user.id)
+        @current_entry = Entry.create(user_id, :room_id: @room.id)
+        @another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
+        redirect_to room_path(@room)
+    end
 end
