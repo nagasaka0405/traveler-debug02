b185a1b09d77cde60de851ae3447f27b9f90b227
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-11-05 22:49:57 +0900

    [add]DM機能（メッセージコントローラー追加）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+    before_action:authenticate_user!, :only => [:create]
+
+    def create
+        message = Message.new(message_params)
+        message.user_id = current_user.id
+        if message.save
+            redirect_to room_path(message.room)
+        else
+            redirect_back(fallback_location: root_path)
+        end
+    end 
 end
