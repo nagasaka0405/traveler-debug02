4c5982752b7065a8d41839ee7fb21f36c39d064b
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-11-12 21:58:58 +0900

    [fix]制限文字数修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
-  validates :message, presence: true, length: { maximum: 140 }
+  validates :message, presence: true, length: { maximum: 130 }
   #　メッセージは空欄はNG、かつ140字以内
 end
